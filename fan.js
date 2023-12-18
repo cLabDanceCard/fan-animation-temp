9cc82dda94ae924b909c1a67d254b9dcaabe9d64
Danny Liu <dannyjialiliu@gmail.com>
2023-12-18 10:24:58 -0800

    fix visibility logic

--- fan.js
+++ fan.js
@@ -1,15 +1,15 @@
 var leftBar = document.getElementById("bar1");
 var rightBar = document.getElementById("bar2");
 
-var allSlices = document.querySelectorAll(".pie-slice");
+var allSlices = document.querySelectorAll("*");
 
 leftBar.addEventListener("click", function () {
 	allSlices.forEach(function (slice) {
         if (slice.classList.contains("left-border")){
-            slice.style.border = slice.style.border === "none" ? "flex" : "none";
+            slice.style.border = slice.style.border === "none" ? "" : "none";
         }
         else if (slice.classList.contains("left")) {
-            slice.style.display = slice.style.display === "none" ? "flex" : "none";
+            slice.style.display = slice.style.display === "none" ? "" : "none";
         }
 	});
 });
@@ -18,11 +18,11 @@ rightBar.addEventListener("click", function () {
 	allSlices.forEach(function (slice) {
         if (slice.classList.contains("right-border")){
             console.log("right border")
-            slice.style.border = slice.style.border === "none" ? "flex" : "none";
+            slice.style.border = slice.style.border === "none" ? "" : "none";
         }
         else if (slice.classList.contains("right")) {
             console.log("right")
-            slice.style.display = slice.style.display === "none" ? "flex" : "none";
+            slice.style.display = slice.style.display === "none" ? "" : "none";
         }
 	});
 });
